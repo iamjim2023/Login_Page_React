9b84f2f81aef194aabb8d4bb140ca4b387e83160
Jimuel Madlangsakay <jmuelkoh22@gmail.com>
2023-02-27 09:31:58 +0800

    update

---
+++ src/utils/database.js
@@ -0,0 +1,14 @@
+export const database = [
+  {
+    username: "admin",
+    password: "admin",
+  },
+  {
+    username: "employee",
+    password: "employee",
+  },
+  {
+    username: "guest",
+    password: "guest",
+  },
+];
